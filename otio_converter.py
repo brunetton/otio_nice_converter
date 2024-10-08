8c1223c158c9359762df9abb122f3e438a1f8c62
Bruno Duyé <86322+brunetton@users.noreply.github.com>
2024-10-08 12:57:33 +0200

    Catch exception if no adapter is found and display a nice message

--- otio_converter.py
+++ otio_converter.py
@@ -15,6 +15,7 @@ Options:
 import opentimelineio as otio
 from pathlib import Path
 from docopt import docopt
+import opentimelineio.exceptions
 
 
 args = docopt(__doc__.format(self_filename=Path(__file__).name))
@@ -25,8 +26,16 @@ if args["--list_adapters"]:
 
 input_file = Path(args["<input_file>"])
 
-# read input file
-timeline = otio.adapters.read_from_file(input_file)
+try:
+    # read input file
+    timeline = otio.adapters.read_from_file(input_file)
+except opentimelineio.exceptions.NoKnownAdapterForExtensionError as e:
+    print(
+        f"{e}\nYou can try to install a missing adapter (pip install otio-XXXX-adapter)"
+    )
+    print(f"Currently installed adapters: {otio.adapters.available_adapter_names()}")
+    # EDL: pip install otio-cmx3600-adapter
+    # XML: pip install otio-fcpx-xml-adapter
 
 # write OTIO file
 output_file = input_file.parent / Path(input_file.stem + ".otio")
