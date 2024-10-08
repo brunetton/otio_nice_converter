d5fec70593bb91f7a2650af717e929c0442dec83
Bruno Duyé <86322+brunetton@users.noreply.github.com>
2024-10-08 13:04:48 +0200

    Add --adapter option + code reformat

--- otio_converter.py
+++ otio_converter.py
@@ -10,6 +10,7 @@ Usage:
 
 Options:
     --list_adapters                 List installed adapters and exit
+    --adapter=<adapter_name>        Use given adapter (must be listed in --list_adapters)
 """
 
 import opentimelineio as otio
@@ -20,20 +21,28 @@ import opentimelineio.exceptions
 
 args = docopt(__doc__.format(self_filename=Path(__file__).name))
 
-if args["--list_adapters"]:
+
+def list_adapters_and_exit():
     print(f"Installed OTIO adpaters: {otio.adapters.available_adapter_names()}")
     exit(0)
 
+
+if args["--list_adapters"]:
+    list_adapters_and_exit()
+
 input_file = Path(args["<input_file>"])
+adapter_name = args.get("--adapter")
+if adapter_name:
+    print(f"Using adapter: {adapter_name}")
 
 try:
     # read input file
-    timeline = otio.adapters.read_from_file(input_file)
+    timeline = otio.adapters.read_from_file(input_file, adapter_name=adapter_name)
 except opentimelineio.exceptions.NoKnownAdapterForExtensionError as e:
     print(
-        f"{e}\nYou can try to install a missing adapter (pip install otio-XXXX-adapter)"
+        f"{e}\nYou can try to use the --adapter option to focrce using a specific adapter, or install a missing adapter (pip install otio-XXXX-adapter)"
     )
-    print(f"Currently installed adapters: {otio.adapters.available_adapter_names()}")
+    list_adapters_and_exit()
     # EDL: pip install otio-cmx3600-adapter
     # XML: pip install otio-fcpx-xml-adapter
 
